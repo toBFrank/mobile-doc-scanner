267c1985fa31b6ac06466150cacc941e1911207b
Franco Bonilla <bonilla.franco484@gmail.com>
2025-06-30 09:11:19 -0500

    Perspective transform done

--- scan.py
+++ scan.py
@@ -43,3 +43,12 @@ cv2.drawContours(image, [screenCnt], -1, (0, 255, 0), 2)
 cv2.imshow("Outline", image)  # outline of the document
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+print("#3. Apply Perspective Transform")
+warped = four_point_transform(orig, screenCnt.reshape(4, 2) * ratio)
+thresh_loc = threshold_local(warped, 11, offset=10, method="gaussian")
+warped = (warped > thresh_loc).astype("uint8") * 255
+
+cv2.imshow("Original", imutils.resize(orig, height = 650))  # original image
+cv2.imshow("Scanned", imutils.resize(warped, height = 650))  # scanned image
+cv2.waitKey(0)
